c5921d1c07401f43bb630ae02176cdca2e78be1b
koaku <kouakoubarbier@gmail.com>
2019-08-20 11:06:09 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
