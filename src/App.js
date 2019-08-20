223ce4a664237faecff3b1760856539bbd697783
koaku <kouakoubarbier@gmail.com>
2019-08-20 16:09:10 +0100

    shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/homepage/homepage.component";
-
+import Home from "./pages/homepage/homepage-component";
+import Shop from "./pages/shop/shop-component";
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage-component.jsx
+++ src/pages/homepage/homepage-component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./homepage.styles.scss";
+import "./homepage-styles.scss";
 import Directory from "../../components/directory/directory-component";
 export default function Home() {
   return (
